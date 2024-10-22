74dca625189b3087a26f18698d3cc53c19992523
AxeemHaider <axeemhaider@gmail.com>
2024-10-22 10:22:04 +0500

    remove extra scripts

--- maintenance.sh
+++
@@ -1,5 +0,0 @@
-journalctl --vacuum-time=10d --vacuum-size=500M;
-/opt/renew-vm-cert.sh;
-cd /opt/elestio/nginx && docker-compose down;
-cd /opt/elestio/nginx && docker-compose up -d;
-docker image prune -a -f
