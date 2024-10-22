c6690e5c0b5ba7c42568797b2998cc3ab1afb14f
AxeemHaider <axeemhaider@gmail.com>
2024-10-23 00:25:04 +0500

    adding super set docker

--- superset/scripts/preInstall.sh
+++ superset/scripts/preInstall.sh
@@ -1,7 +1,6 @@
-# Set environment variables
+set env vars
 set -o allexport; source .env; set +o allexport;
 
-# Create necessary directories and set permissions
 mkdir -p ./docker
 chown -R 1000:1000 ./docker
 
@@ -14,11 +13,10 @@ chown -R 1000:1000 ./redis
 mkdir -p ./db_home
 chown -R 1000:1000 ./db_home
 
+chmod +x ./docker
+chmod +x ./docker/*.sh
 
-# Create the directory for superset config if it doesn't exist
-mkdir -p ./docker/pythonpath_dev
 
-# Create the superset configuration file
 cat << EOF > ./docker/pythonpath_dev/superset_config_docker.py
 SECRET_KEY = '${ADMIN_PASSWORD}'
 EMAIL_NOTIFICATIONS = False
@@ -27,12 +25,6 @@ SUPERSET_WEBSERVER_PROTOCOL = 'https'
 APP_NAME = 'Superset'
 EOF
 
-touch ./docker/docker-init.sh
 
-# Apply changes to docker-init.sh if the file exists
-if [ -f ./docker/docker-init.sh ]; then
-    sed -i "s~--password ~--password ${ADMIN_PASSWORD}~g" ./docker/docker-init.sh
-    sed -i "s~--email ~--email ${ADMIN_EMAIL}~g" ./docker/docker-init.sh
-else
-    echo "Warning: ./docker/docker-init.sh not found"
-fi
+sed -i "s~--password ~--password ${ADMIN_PASSWORD}~g" ./docker/docker-init.sh
+sed -i "s~--email ~--email ${ADMIN_EMAIL}~g" ./docker/docker-init.sh
